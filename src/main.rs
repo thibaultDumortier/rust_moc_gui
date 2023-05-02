8f85e527ebb5bca39d51dee73f96ce769e3f0e5a
DUMORTIER THIBAULT <thibault.dumortier@astro.unistra.fr>
2023-05-02 09:37:19 +0000

    fixed issue concerning name on start_web

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
         ..Default::default()
     };
     let _ = eframe::run_native(
-        "moc app",
+        "Moc app",
         options,
         Box::new(|_| Box::new(rust_moc_gui::FileApp::default())),
     ); // This is supposed to work, if it does not, then there is a greater error, please open an issue.
